c683f36fbafc5e5d6584a1621ed3a895662aad6d
Marko Topolnik <marko.topolnik@gmail.com>
2026-03-28 13:50:45 +0100

    Larger min SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.grandfatherclock"
-        minSdk = 26
+        minSdk = 30
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
